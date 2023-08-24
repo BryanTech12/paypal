da7b833103a324f7ea35c6a87d91869db006f33d
bryan <chahbriyan@yahoo.com>
2023-08-24 11:13:24 -0700

    done

--- app.java
+++ app.java
@@ -4,4 +4,6 @@ for paypal webapp
 ready dor deployment in production
 fir paypal wepadd
 staging
-
+fir paypal wepadd
+staging
+>>>>>>> 1a993aa (app.java modified)
--- list.tst
+++ list.tst
@@ -1,3 +1,4 @@
 simon
 jame
 anna
+vivian
